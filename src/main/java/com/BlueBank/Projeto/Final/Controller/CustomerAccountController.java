dc44303cdacccc2f1cefd3bc898b083be5ec4616
Renata-CDB <renatabianchine2016@gmail.com>
2022-04-02 09:20:23 -0300

    modificaçoes

--- src/main/java/com/BlueBank/Projeto/Final/Controller/CustomerAccountController.java
+++ src/main/java/com/BlueBank/Projeto/Final/Controller/CustomerAccountController.java
@@ -1,8 +1,15 @@
 package com.BlueBank.Projeto.Final.Controller;
 
+/*import java.awt.print.Pageable;*/
+
+/*import org.springframework.data.domain.Page;*/
 import org.springframework.http.ResponseEntity;
+/*import org.springframework.web.bind.annotation.DeleteMapping;*/
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+/*import org.springframework.web.bind.annotation.PostMapping;*/
+/*import org.springframework.web.bind.annotation.PutMapping;*/
+/*import org.springframework.web.bind.annotation.RequestBody;*/
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,13 +29,41 @@ public class CustomerAccountController
 		this.customerAccountService = customerAccountService;
 	}
 	
+	/*@GetMapping
+	public ResponseEntity<Page<CustomerAccount>> getAll(Pageable pageable){
+		 Page<CustomerAccount> listaCustomerAccount = customerAccountService.findAll(pageable);		
+		return ResponseEntity.ok().body(listaCustomerAccount);
+	}*/
+	
 	@GetMapping("/{id}")
-	public ResponseEntity<CustomerAccount> getById(@PathVariable Long id) 
-	{
+	public ResponseEntity<CustomerAccount> getById(@PathVariable Long id){ 
 		CustomerAccount customerAccount = customerAccountService.getById(id);
 		return ResponseEntity.ok(customerAccount);
 		
 	}
 	
-}   
+	/*@PostMapping
+	public ResponseEntity<CustomerAccount> create(@RequestBody CustomerAccount customerAccount){
+		CustomerAccount c = customerAccountService.create((customerAccount);)
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(c.getId()).toUri();	
+		
+		return ResponseEntity.created(location).body(c);
+	}*/
+	
+   /*@PutMapping("/{id}")
+    public ResponseEntity<CustomerAccount> update(@RequestBody CustomerAccount CustomerAccount, @PathVariable Long id){
+	   CustomerAccount = customerAccountService.update(customerAccount, id);
+	//return ResponseEntity.noContent().build();
+	return ResponseEntity.ok().body(customerAccount);
+	}*/
+	
+	/*@DeleteMapping("/{id}")
+	public ResponseEntity<customerAccount> delete(@PathVariable Long id){
+		customerAccountService.delete(id);
+		return ResponseEntity.noContent().build();
+	}*/
+   
+   }
+
 
--- src/main/java/com/BlueBank/Projeto/Final/Service/CustomerAccountService.java
+++ src/main/java/com/BlueBank/Projeto/Final/Service/CustomerAccountService.java
@@ -1,9 +1,10 @@
 package com.BlueBank.Projeto.Final.Service;
 
 
+/*import java.awt.print.Pageable;*/
 import java.util.Optional;
 
-
+/*import org.springframework.data.domain.Page;*/
 import org.springframework.stereotype.Service;
 
 import com.BlueBank.Projeto.Final.Model.CustomerAccount;
@@ -12,14 +13,20 @@ import com.BlueBank.Projeto.Final.Repository.CustomerAccountRepository;
 @Service
 public class CustomerAccountService 
 {
-	private CustomerAccountRepository customerAccountRepository;
-	public CustomerAccountService (CustomerAccountRepository customerAccountRepository) 
+	
+    private CustomerAccountRepository customerAccountRepository;
+	
+    public CustomerAccountService (CustomerAccountRepository customerAccountRepository) 
 	{
 		this.customerAccountRepository =customerAccountRepository;
 	}
 	
 	    
-   
+   //Paginado
+	/*public Page<CustomerAccount> findAll(Pageable pageable){
+	   	Page<CustomerAccount> list = CustomerAccountRepository.findAll(pageable);
+	   	return list;
+	}*/
 			
 	
 	public CustomerAccount getById(Long id) 
